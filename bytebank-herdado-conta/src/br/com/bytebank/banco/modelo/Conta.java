4df5fb3c466203fb3c0d63ffe36fb6bbac736559
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2022-12-27 23:54:30 -0300

    Equals e mais listas

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -6,7 +6,7 @@ package br.com.bytebank.banco.modelo;
  * @author Derivaldo Brito
  *
  */
-public abstract class Conta {
+public abstract class Conta extends Object{
 
     protected double saldo;
     private int agencia;
@@ -89,6 +89,22 @@ public abstract class Conta {
     public static int getTotal(){
         return Conta.total;
     }
+    
+    @Override
+    public boolean equals(Object ref) {
+    	
+    	Conta outra = (Conta) ref;
+    	
+    	if(this.agencia != outra.agencia) {
+    		return false;
+    	}
+    	if(this.numero != outra.numero) {
+    		return false;
+    	}
+    	
+    	return true;
+    }
+    
     @Override
 	public String toString() {
 		return "Numero: " + this.numero +", Agencia: " + this.agencia;
